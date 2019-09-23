07eaf5e73945f6dd2be28628719fb4a1e8fa32c3
Nestor hdez <nestorhdez.26@gmail.com>
2019-09-23 20:36:46 +0100

    Profile view, table of regattas and loading message.

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Contact from './views/Contact.vue'
+import Profile from './views/Profile.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/contact',
       name: 'contact',
       component: Contact,
+    },
+    {
+      path: '/profile/:id',
+      name: 'profile',
+      component: Profile
     }
   ]
 })
